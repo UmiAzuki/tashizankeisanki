ea49eea681e1139deed9a476f890dfbcd6d503ef
UmiAzuki <asuwandamu@gmail.com>
2023-09-14 01:09:43 +0900

    足し算の計算をできるようにした

--- tashizan-app/src/index.php
+++ tashizan-app/src/index.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+
 <?php
-echo "Hello World!";
-phpinfo();
+    $answer = 0;
+    if(isset($_POST["button_sum"])){
+        $left = $_POST["left"];
+        $right = $_POST["right"];
+        $answer = $left + $right;
+    }
+?>
+
+<html lang="ja">
+    <head>
+        <title>足し算する電卓</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <form action="" method="post">
+            <p>
+            <input type="text" name="left" value="<?php echo $left; ?>"> +
+            <input type="text" name="right" value="<?php echo $right; ?>"> =
+            <span><?php echo $answer; ?></span>
+            <input type="submit" name="button_sum" value="計算">
+            </p>
+        </form>
+    </body>
+</html>
